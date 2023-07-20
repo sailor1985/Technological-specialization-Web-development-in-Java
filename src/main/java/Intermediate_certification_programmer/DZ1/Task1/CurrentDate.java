fdf1421db469f07a5313f95bde07d61faa31807c
Michael <cadetvire@rambler.ru>
2023-07-20 17:11:01 +0300

    Refactor: DZ2->DZ1

---
+++ src/main/java/Intermediate_certification_programmer/DZ1/Task1/CurrentDate.java
@@ -0,0 +1,15 @@
+package Intermediate_certification_programmer.DZ1.Task1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CurrentDate {
+    public String currentDate() {
+        // Получить текущую дату
+        Date date = new Date();
+        // Задать формат даты
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        return sdf.format(date);
+    }
+}
+
