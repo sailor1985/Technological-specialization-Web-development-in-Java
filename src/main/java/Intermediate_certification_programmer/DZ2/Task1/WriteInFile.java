44340a412efe31d2c1ede998392a816dc42c9b99
Michael <cadetvire@rambler.ru>
2023-07-20 17:08:09 +0300

    Task2 - done!

---
+++ src/main/java/Intermediate_certification_programmer/DZ2/Task1/WriteInFile.java
@@ -0,0 +1,17 @@
+package Intermediate_certification_programmer.DZ2.Task1;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+public class WriteInFile {
+    public void writeInFile(String filename, String noteToFile) {
+        // Инициализировать объект файла
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            writer.write(noteToFile);
+            writer.newLine();
+            System.out.println("Строка успешно записана в файл.");
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи в файл: " + e.getMessage());
+        }
+    }
+}
