1b6bad2752706ab9067efaa8d3eb8cec9ddf00ca
Michael <cadetvire@rambler.ru>
2023-08-05 17:16:08 +0300

    HTML_CSS DZ1 - Done!

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Test {
+public class Test {
+    public static void main(String[] args) {
+        final StringBuffer a = new StringBuffer();
+        final StringBuffer b = new StringBuffer();
+
+        new Thread(() -> {
+            System.out.println(a.append("A"));
+            synchronized (b) {
+                System.out.println(b.append("B"));
+            }
+        }).start();
+
+        new Thread(() -> {
+            System.out.println(b.append("C"));
+            synchronized (a) {
+                System.out.println(a.append("D"));
+            }
+        }).start();
+    }
 }
